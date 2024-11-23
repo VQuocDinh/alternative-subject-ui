679e4dd1dcb2021ed5d664f6cb74da542dc3bc3b
voquocdinh <vqdinh2202@@gmail.com>
2024-11-23 22:44:03 +0700

    config vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vitejs.dev/config/
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -28,5 +32,14 @@ export default defineConfig({
     proxy: {
       '/api': 'http://localhost:8080'
     }
-  }
+  },
+
+  // Cấu hình alias
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@App': path.resolve(__dirname, 'src')
+    },
+  },
 })
+
