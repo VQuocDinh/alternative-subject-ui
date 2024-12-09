3709da0d29ec34d76b81fd7a6e81ef05afc894c9
ndtien142 <ndtien142@gmail.com>
2024-12-09 22:07:26 +0700

    create axios instance

---
+++ src/common/utils/axios.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+export const baseUrl = import.meta.env.VITE_BASE_URL || 'http://localhost:8080/api';
+
+const axiosInstance = axios.create({
+  baseURL: baseUrl, // Replace with your API base URL
+  timeout: 10000, // Request timeout in milliseconds
+  headers: {
+    'Content-Type': 'application/json',
+    // Add any other custom headers here
+  },
+});
+
+// Add a request interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // Do something before request is sent
+    // For example, add an authorization token
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+axiosInstance.interceptors.response.use(
+  (response) => {
+    // Do something with response data
+    return response;
+  },
+  (error) => {
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
